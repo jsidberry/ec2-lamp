d8ab1d796b0d3f9378e122dedf04078df22fe7e0
Juan Sidberry <jsidberry@gmail.com>
2018-11-08 08:50:47 -0500

    add playbook and roles for apache and php-7

---
+++ roles/php/files/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Home Page</title>
+</head>
+<body>
+	<h1><?php echo "Welcome to DevOps for AWS. This text was generated using PHP 7" ?></h1>
+</body>
+</html>
